4293a1ff648583efc0ec8d0992eb13d0958604d1
DanisValitov <fermodil@mail.ru>
2023-02-02 11:42:23 +0300

    dz2

---
+++ d2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Console.WriteLine("write a: ");
+// String a = Console.ReadLine();
+// if(a.Length != 3) throw new System.Exception("len is not 3");
+// int ai = Int16.Parse(a);
+// char target = a[1];
+// String res = a + " -> " + target;
+// Console.WriteLine(res);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.WriteLine("write a: ");
+// String a = Console.ReadLine();
+// int ai = Int16.Parse(a);
+// String target =  (a.Length >2) ?  a[2].ToString() : "третьей цифры нет";
+// String res = a + " -> " + target;
+// Console.WriteLine(res);
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.WriteLine("write a: ");
+// String a = Console.ReadLine();
+// int ai = Int16.Parse(a);
+// String res = "да";
+// switch (ai) {
+//     case 6 : 
+//     case 7 :
+//         break;
+//     default:
+//         res = "нет";
+//         break;
+// }
+// Console.WriteLine(a + " -> " + res);
